d51bc0e6c0687132d31fe0a0dc20cb37e7bafe30
caozhujia <caozhujia@proton.me>
2023-12-28 05:40:34 +0000

    modernized

--- draw.py
+++ draw.py
@@ -45,4 +45,4 @@ while True:
                          new_gray, 
                          old_pts, 
                          None, maxLevel=1,
-                         criteria=
+                         criteria=(cv
