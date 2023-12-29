e7e4748745a8f157baea94152d46fadf5d9d8ab9
caozhujia <caozhujia@proton.me>
2023-12-29 11:55:10 +0000

    f1

--- draw.py
+++ draw.py
@@ -45,4 +45,4 @@ while True:
                          new_gray, 
                          old_pts, 
                          None, maxLevel=1,
-                         criteria=(cv2.TERM_CRI
+                         criteria=(cv2.TERM_CRITE
