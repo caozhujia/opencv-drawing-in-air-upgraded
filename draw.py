d8090d7c6a8713130eea8be85328cadfe4cf603b
caozhujia <caozhujia@proton.me>
2023-12-29 17:23:47 +0000

    Update catalogue_articles.py

--- draw.py
+++ draw.py
@@ -45,4 +45,4 @@ while True:
                          new_gray, 
                          old_pts, 
                          None, maxLevel=1,
-                         criteria=(cv2.TERM_CRITERIA_
+                         criteria=(cv2.TERM_CRITERIA_EP
