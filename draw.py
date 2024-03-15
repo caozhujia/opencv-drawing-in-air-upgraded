f0d4615d91fef6812e3ee9e26b4f3e987d48e35c
caozhujia <caozhujia@proton.me>
2024-03-15 15:46:33 +0000

    Meta

--- draw.py
+++ draw.py
@@ -67,4 +67,4 @@ while True:
             x,y = j.ravel()
             a, b = i.ravel()
 
-            cv2.line(mask, (int(a),int(b)), (int(x), 
+            cv2.line(mask, (int(a),int(b)), (int(x), i
